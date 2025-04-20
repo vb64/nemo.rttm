b3261b740f6c6ace2b73395e08254bd84bc169b2
vb64 <vit.sar68@gmail.com>
2025-04-20 12:04:02 +0400

    files

---
+++ makefile
@@ -0,0 +1,36 @@
+.PHONY: all setup
+# make >debug.log 2>&1
+# git remote prune origin
+ifeq ($(OS),Windows_NT)
+PYTHON = venv/Scripts/python.exe
+PTEST = venv/Scripts/pytest.exe
+else
+PYTHON = ./venv/bin/python
+PTEST = ./venv/bin/pytest
+endif
+
+SOURCE = source
+
+FLAKE8 = $(PYTHON) -m flake8
+PYLINT = $(PYTHON) -m pylint
+PYTEST = $(PTEST) --cov=$(SOURCE) --cov-report term:skip-covered
+PIP = $(PYTHON) -m pip install
+
+
+all:
+	$(PYTHON) -m pydocstyle $(SOURCE)
+	$(FLAKE8) $(SOURCE)
+	$(PYLINT) $(SOURCE)
+
+rttm:
+	$(PYTHON) $(SOURCE)/to_rttm.py
+
+setup: setup_python setup_pip
+
+setup_pip:
+	$(PIP) --upgrade pip
+	$(PIP) -r requirements.txt
+	$(PIP) -r dev.txt
+
+setup_python:
+	$(PYTHON_BIN) -m venv ./venv
---
+++ source/to_rttm.py
@@ -0,0 +1,40 @@
+"""Make Nemo rttm files."""
+import os
+import sys
+import time
+
+VERSION = '1.0'
+COPYRIGHTS = 'Copyrights by Vitaly Bogomolov 2025'
+PARSER = argparse.ArgumentParser(description='Nemo diarize tool.')
+
+PARSER.add_argument(
+  "wav_file",
+  help="Wav file for diarize."
+)
+PARSER.add_argument(
+  "--num_speakers",
+  type=int,
+  default=0,
+  help="Forcing the number of speakers, default 0 (auto detection)",
+)
+PARSER.add_argument(
+  "--temp_folder",
+  default='temp',
+  help="Folder for temp files.",
+)
+PARSER.add_argument(
+  "--config",
+  default='nemo.cfg',
+  help="Path to Nemo config file.",
+)
+
+
+def main(options):
+    print("Nemo diarization tool v.{}. {}".format(VERSION, COPYRIGHTS))
+    start_time = time.time()
+    dest = os.path.splitext(options.wav_file)[0] + '.rttm'
+    print(dest, "{} sec".format(int(time.time() - start_time)))
+
+
+if __name__ == '__main__':  # pragma: no cover
+    sys.exit(main(PARSER.parse_args()))
