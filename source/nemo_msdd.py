682cf019705cd818eb9ba51bf44474e5d1cfece7
vb64 <vit.sar68@gmail.com>
2025-04-22 22:40:40 +0400

    step

--- source/nemo_msdd.py
+++ source/nemo_msdd.py
@@ -66,6 +66,9 @@ def create_config(wav_file, output_dir, config_path, num_speakers=0):
     return config
 
 
+# nemo/collections/asr/parts/utils/offline_clustering.py", line 553, in eigDecompose
+# lambdas, diffusion_map = eigh(laplacian)
+# https://github.com/pytorch/pytorch/issues/68291
 def diarize(wav_file, device, num_speakers, temp_path, config_path):
     """Initialize NeMo MSDD diarization model."""
     from nemo.collections.asr.parts.preprocessing.segment import AudioSegment
@@ -76,5 +79,5 @@ def diarize(wav_file, device, num_speakers, temp_path, config_path):
 
     config = create_config(wav_file, temp_path, config_path, num_speakers=num_speakers)
     model = NeuralDiarizer(cfg=config).to(device)
-
-    return model.diarize()
+    print("###", model.cfg_diar_infer.batch_size)
+    return 0  # model.diarize()
