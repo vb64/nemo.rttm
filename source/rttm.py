6f24b10d086189672c01e5ddc0dbb795f7be1e76
vb64 <vit.sar68@gmail.com>
2025-06-04 21:21:21 +0400

    step

--- source/rttm.py
+++ source/rttm.py
@@ -1,5 +1,4 @@
 """RTTM file operations."""
-import os
 
 
 class NemoRow:
@@ -7,18 +6,18 @@ class NemoRow:
 
     # SPEAKER xxx.mp3 1   8.940   6.060 <NA> <NA> speaker_0 <NA> <NA>
     row_template = "SPEAKER {} 1 {:.3f} {:.3f} <NA> <NA> speaker_{} <NA> <NA>\n"
-    file_name = ''
 
-    def __init__(self):
+    def __init__(self, rttm_file):
         """Make empty row."""
         self.start = None
         self.length = None
         self.speaker = None
+        self.file_index = rttm_file.file_index
 
     @classmethod
-    def from_line(cls, line):
+    def from_line(cls, line, rttm_file):
         """Load data from file."""
-        obj = cls()
+        obj = cls(rttm_file)
         fields = line.split()
         obj.start = int(float(fields[3]) * 1000)
         obj.length = int(float(fields[4]) * 1000)
@@ -30,7 +29,7 @@ class NemoRow:
     def line(self):
         """Return data as string."""
         return self.row_template.format(
-          self.file_name,
+          self.file_index,
           self.start / 1000.0,
           self.length / 1000.0,
           self.speaker
@@ -46,21 +45,22 @@ class NemoRttm:
         """Make empty file."""
         self.rows = []
         self.length_ms = 0
+        self.file_index = 0
 
     @classmethod
-    def from_file(cls, file_name, length_ms):
+    def from_file(cls, file_name, file_index, length_ms):
         """Load data from file."""
         obj = cls()
         obj.length_ms = length_ms
+        obj.file_index = file_index
         with open(file_name, 'r', encoding=cls.encoding) as inp:
             for line in inp:
-                obj.rows.append(NemoRow.from_line(line))
+                obj.rows.append(NemoRow.from_line(line, obj))
 
         return obj
 
     def save(self, file_name):
         """Save data to file."""
-        NemoRow.file_name = os.path.basename(file_name)
         with open(file_name, 'wt', encoding=self.encoding) as out:
             for row in self.rows:
                 out.write(row.line)
@@ -85,8 +85,8 @@ class NemoRttm:
 def join_rttms(rttms):
     """Join rttm files from list to single rttm file."""
     name, length = rttms[0]
-    rttm = NemoRttm.from_file(name, length)
-    for name, length in rttms[1:]:
-        rttm.append(NemoRttm.from_file(name, length))
+    rttm = NemoRttm.from_file(name, length, 0)
+    for i, (name, length) in enumerate(rttms[1:], start=1):
+        rttm.append(NemoRttm.from_file(name, length, i))
 
     return rttm
